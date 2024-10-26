1669796a3655e99a5a214e658b733cd8f1b38053
RamaRefin <Rama.223040040@mail.unpas.ac.id>
2024-10-26 22:59:45 +0700

    Struktur folder

---
+++ resources/views/about.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman About Lion</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+
+<body>
+
+<a href="/">Home</a>
+<a href="/about">about</a>
+
+<h1> Halaman About </h1>
+<h3> Owner : {{ $nama }} </h3>
+<img src="img/lion.jpg" alt="lion" width="200">
+    
+</body>
+</html>
